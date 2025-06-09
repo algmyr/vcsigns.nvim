0d9dd29a6b0346ebcae67edd9c1b600f880e2ef0
Anton Älgmyr <anton@algmyr.se>
2025-06-09 11:36:39 +0200

    Hunk textobj function.

---
+++ lua/vcsigns/textobj.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+function M.select_hunk(bufnr)
+  local lnum = vim.fn.line "."
+  local hunks = vim.b[bufnr].vcsigns_hunks
+  local hunk = require("vcsigns").diff.cur_hunk(lnum, hunks)
+  if not hunk then
+    vim.notify(
+      "No hunk under cursor",
+      vim.log.levels.WARN,
+      { title = "VCSigns" }
+    )
+    return
+  end
+  local start = hunk.plus_start
+  local finish = start + hunk.plus_count - 1
+  vim.cmd(string.format("normal! %dG%d|V%dG", start, start, finish))
+end
+
+return M
